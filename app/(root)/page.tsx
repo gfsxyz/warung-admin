7ea7bf5867737e74bedaf484e0922a923809e6d3
gfsxyz <gustifaizals@gmail.com>
2023-07-15 03:09:34 +0800

    setup auth

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,10 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
-export default function Home() {
+export default function SetupPage() {
   return (
-    <div>
-      <Button>Start</Button>
+    <div className="p-4">
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 }
